c4bd9a94120fff23ef7a4b62baa150c18ff55b13
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-10-08 01:56:33 -0300

    Added teacher app, teacher model and migration files

---
+++ backend/teacher/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    value = models.DecimalField(max_digits=9, decimal_places=2, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    photo = models.URLField(max_length=255, null=False, blank=False)
