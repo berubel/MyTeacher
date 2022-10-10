a15e29db16451cab5d40320b8565f65a7bedc280
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-10-10 19:12:54 -0300

    Updated corsheaders

--- backend/myteacher/settings.py
+++ backend/myteacher/settings.py
@@ -38,14 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teacher.apps.TeacherConfig',
-    'rest_framework',
     "corsheaders",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
