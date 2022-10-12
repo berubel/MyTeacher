637ecb44611b926727efe05c0aca8e7b7571319a
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-10-12 17:23:09 -0300

    Show dialog with fields and buttons

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Dialog, DialogActions, Grid, TextField, Button } from '@mui/material'
 import type { NextPage } from 'next'
 import { Teacher } from '../src/@types/teacher'
 import List from '../src/components/List/List'
@@ -8,9 +8,35 @@ const Home: NextPage = () => {
   const {teacherList} = useIndex();
 
   return (
-    <Box sx = {{backgroundColor: 'secondary.main'}}>
-      <List teachers = {teacherList}></List>  
-    </Box>
+   <div>
+      <Box sx = {{backgroundColor: 'secondary.main'}}>
+        <List teachers = {teacherList}></List>  
+      </Box>
+      <Dialog open={true} fullWidth PaperProps={{sx: {p:5}}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Name"
+              type= "text"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Email"
+              type= "text"
+              fullWidth
+            />
+            </Grid>
+        </Grid>
+
+        <DialogActions sx={{mt: 5}}>
+          <Button>Cancel</Button>
+          <Button>Book</Button>
+        </DialogActions>
+      </Dialog>
+
+   </div>
     
   )
 }
